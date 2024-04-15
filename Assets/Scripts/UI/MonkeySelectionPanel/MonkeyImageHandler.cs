712cda8c01731ef3c96dbfd2c53e96a9251e450e
Chetan Rautela <chetanrautela20@gmail.com>
2024-04-15 13:31:10 +0530

    Update MonkeyImageHandler.cs

--- Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
+++ Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
@@ -31,7 +31,7 @@ namespace ServiceLocator.UI
 
         public void OnDrag(PointerEventData eventData)
         {
-            rectTransform.anchoredPosition += eventData.delta;
+            rectTransform.position += (Vector3)eventData.delta;
             owner.MonkeyDraggedAt(rectTransform.position);
         }
 

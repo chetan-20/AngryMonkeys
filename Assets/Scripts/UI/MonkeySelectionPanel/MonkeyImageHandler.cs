03eb97c1263498ceb36dfd4185ba59862700ec46
Chetan Rautela <chetanrautela20@gmail.com>
2024-04-15 13:04:59 +0530

    spawning of the monkey

--- Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
+++ Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
@@ -6,18 +6,21 @@ using UnityEngine.UI;
 
 namespace ServiceLocator.UI
 {
-    public class MonkeyImageHandler : MonoBehaviour,IDragHandler
+    public class MonkeyImageHandler : MonoBehaviour,IDragHandler,IEndDragHandler,IPointerDownHandler
     {
         private Image monkeyImage;
         private MonkeyCellController owner;
         private Sprite spriteToSet;
         private RectTransform rectTransform;
-           
+        private Vector3 originalposition;
+        private Vector3 originanchoredposition;   
         private void Awake()
         {
             monkeyImage = GetComponent<Image>();
             monkeyImage.sprite = spriteToSet;
-            rectTransform = GetComponent<RectTransform>();  
+            rectTransform = GetComponent<RectTransform>();
+            originalposition = rectTransform.position;
+            originanchoredposition = rectTransform.anchoredPosition;
         }
 
         public void ConfigureImageHandler(Sprite spriteToSet, MonkeyCellController owner)
@@ -30,6 +33,26 @@ namespace ServiceLocator.UI
         {
             rectTransform.anchoredPosition += eventData.delta;
             owner.MonkeyDraggedAt(rectTransform.position);
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            ResetMonkey();
+            owner.MonkeyDroppedAt(eventData.position); 
+        }
+
+        private void ResetMonkey()
+        {
+            monkeyImage.color = new Color(1, 1, 1, 1f);
+            rectTransform.position = originalposition;
+            rectTransform.anchoredPosition = originanchoredposition;
+            GetComponent<LayoutElement>().enabled = false;
+            GetComponent<LayoutElement>().enabled = true;
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            monkeyImage.color = new Color(1, 1, 1, 0.5f);
         }
     }
 }
